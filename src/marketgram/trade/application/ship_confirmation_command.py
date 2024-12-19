a4a0e8f350a27159e09240d0c929893acb14ad7c
filsom <som-fil@mail.ru>
2024-12-19 16:56:30 +0400

    new handlers

---
+++ src/marketgram/trade/application/ship_confirmation_command.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
+
+from marketgram.trade.application.exceptions import ApplicationError
+from marketgram.trade.application.id_provider import IdProvider
+from marketgram.trade.domain.model.p2p.deal_repository import DealsRepository
+
+
+@dataclass
+class ShipConfirmationCommand:
+    deal_id: UUID
+    occurred_at: datetime
+
+
+class ShipConfirmationHandler:
+    def __init__(
+        self,
+        id_provider: IdProvider,
+        deals_repository: DealsRepository
+    ) -> None:
+        self._id_provider = id_provider
+        self._deals_repository = deals_repository
+
+    async def handle(self, command: ShipConfirmationCommand) -> None:
+        exists_deal = await self._deals_repository \
+            .unshipped_with_id(
+                self._id_provider.provided_id(),
+                command.deal_id
+            )
+        if exists_deal is None:
+            raise ApplicationError()
+        
+        return exists_deal.confirm_shipment(command.occurred_at)
