cabff9730e1f2c87b4eff1ce4667fb700742af70
filsom <som-fil@mail.ru>
2025-01-04 20:02:33 +0400

    Fix

--- src/marketgram/identity/access/main/main.py
+++ src/marketgram/identity/access/main/main.py
@@ -1,15 +1,8 @@
-from datetime import timedelta
-import os
-
 from dishka import Provider
+
 from marketgram.identity.access.main.adapters_provider import AdaptersProvider
 from marketgram.identity.access.main.handlers_provider import HandlersProvider
 from marketgram.identity.access.main.service_provider import ServiceProvider
-from marketgram.identity.access.main.settings import (
-    EmailClientSettings, 
-    JWTManagerSettings, 
-    Settings
-)
 
 
 def identity_access_provider() -> tuple[Provider]:
@@ -17,22 +10,4 @@ def identity_access_provider() -> tuple[Provider]:
         AdaptersProvider(),
         ServiceProvider(),
         HandlersProvider() 
-    )
-
-
-def identity_access_load_settings() -> Settings:
-    email_client = EmailClientSettings(
-        os.environ.get('HOSTNAME'),
-        os.environ.get('PORT'),
-        os.environ.get('USERNAME'),
-        os.environ.get('PASSWORD'),
-        os.environ.get('VALIDATE_CERTS')
-    )
-    jwt_manager = JWTManagerSettings(
-        os.environ.get('JWT_SECRET')
-    )
-    return Settings(
-        email_client,
-        jwt_manager,
-        timedelta(days=15)
     )
