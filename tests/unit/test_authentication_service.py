8d93fd9312c84a60f464e99651968d4fd5054476
filsom <som-fil@mail.ru>
2025-01-11 18:52:22 +0400

    Fix

---
+++ tests/unit/test_authentication_service.py
@@ -0,0 +1,27 @@
+from unittest.mock import Mock
+from uuid import uuid4
+from marketgram.identity.access.domain.model.authentication_service import AuthenticationService
+from marketgram.identity.access.domain.model.user import User
+from marketgram.identity.access.domain.model.user_factory import UserFactory
+from marketgram.identity.access.port.adapter.argon2_password_hasher import Argon2PasswordHasher
+
+
+class TestAuthenticationService:
+    def test_successful_authenticate(self) -> None:
+        # Arrange
+        email = 'test@mail.ru'
+        password = 'unprotected'
+        password_hasher = Argon2PasswordHasher()
+
+        user = User(
+            uuid4(),
+            email,
+            password_hasher.hash(password)
+
+        )
+        user.activate()
+
+        sut = AuthenticationService(password_hasher)
+
+        # Act
+        sut.authenticate(user, password)
