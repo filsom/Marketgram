36ce123cbfd6959d63a81b6590878fcf96e17ddb
filsom <som-fil@mail.ru>
2025-01-11 08:11:45 +0400

    Fix

--- src/marketgram/identity/access/domain/model/web_session.py
+++ src/marketgram/identity/access/domain/model/web_session.py
@@ -1,71 +1,28 @@
+from dataclasses import dataclass
 from datetime import datetime
 from uuid import UUID
-
-
-class WebSession:
-    FORMAT = '%a, %d %b %Y %H:%M:%S'
-
-    def __init__(
-        self,
-        user_id: UUID,
-        session_id: UUID,
-        created_at: datetime,
-        expires_in: datetime,
-        device: str
-    ) -> None:
-        self._user_id = user_id
-        self._session_id = session_id
-        self._created_at = created_at
-        self._expires_in = expires_in
-        self._device = device
-
-    def extend_service_life(
-        self, 
-        new_id: UUID,
-        expires_in: datetime,
-        current_time: datetime
-    ) -> None:
-        difference = self._expires_in - current_time
-
-        if difference.days <= 1:
-            self._session_id = new_id
-            self._created_at = current_time
-            self._expires_in = expires_in
     
+
+@dataclass(frozen=True)
+class WebSession:
+    user_id: UUID
+    session_id: UUID
+    created_at: datetime
+    expires_in: datetime
+    device: str
+
+    def is_living(self, current_time: datetime) -> None:
+        return self.expires_in - current_time <= 1
+
     def to_formatted_time(self) -> str:
-        return self._expires_in.strftime(self.FORMAT)
+        return self.expires_in.strftime('%a, %d %b %Y %H:%M:%S')
     
     def to_string_id(self) -> str:
         return str(self.session_id)
 
     def for_browser(self) -> dict[str, str]:
         return {
-            'user_id': str(self._user_id),
+            'user_id': str(self.user_id),
             'session_id': self.to_string_id(),
             'expires_in': self.to_formatted_time()
         }
-    
-    @property
-    def user_id(self) -> UUID:
-        return self._user_id
-
-    @property
-    def session_id(self) -> UUID:
-        return self._session_id
-    
-    @property
-    def expires_in(self) -> datetime:
-        return self._expires_in
-    
-    @property
-    def created_at(self) -> datetime:
-        return self._created_at
-
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, WebSession):
-            return False
-
-        return self.session_id == other.session_id
-    
-    def __hash__(self) -> int:
-        return hash(self.session_id)
