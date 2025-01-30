4d7b0a76f724d70437d2cc4241ae6ebb065a344c
filsom <som-fil@mail.ru>
2025-01-30 21:20:41 +0400

    Fix

--- src/marketgram/trade/domain/model/p2p/deal/ship_deal.py
+++ src/marketgram/trade/domain/model/p2p/deal/ship_deal.py
@@ -62,9 +62,6 @@ class ShipDeal:
         if self._shipment.is_message():
             raise DomainError()
         
-        if self._download_link is not None:
-            raise DomainError()
-        
         self._download_link = link
 
     def __eq__(self, other: object) -> bool:
