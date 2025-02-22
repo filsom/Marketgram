3df793dc59edd84a714df90402b99360c11de208
filsom <som-fil@mail.ru>
2025-02-22 18:52:50 +0400

    Fix

--- src/marketgram/trade/domain/model/trade_item/card.py
+++ src/marketgram/trade/domain/model/trade_item/card.py
@@ -50,7 +50,6 @@ class EditCard(Card):
         minimum_price: Money,
         minimum_procent_discount: Decimal,
         status: StatusCard,
-        descriptions: list[Description]
     ) -> None:
         super().__init__(card_id)
         self._unit_price = unit_price
@@ -60,7 +59,6 @@ class EditCard(Card):
         self._minimum_price = minimum_price
         self._minimum_procent_discount = minimum_procent_discount
         self._status = status
-        self._descriptions = descriptions
 
     def set_discounted_price(self, new_unit_price: Money) -> None:
         if self._init_price < (self._minimum_price 
@@ -82,25 +80,6 @@ class EditCard(Card):
     def put_on_sale(self) -> None:
         self._status = StatusCard.ON_SALE
 
-    def add_new_description(self, name: str, body: str) -> None: 
-        for description in self._descriptions:
-            if description.status == StatusDescription.NEW:
-                raise DomainError()
-            
-        for field in [name, body]:
-            if len(field) < 10:
-                raise DomainError()
-        
-        self._descriptions.append(
-            Description(
-                self._card_id, 
-                name, 
-                body, 
-                StatusDescription.NEW
-            )    
-        )
-        self._status = StatusCard.ON_MODERATION
-
     def can_add_item(self) -> bool:
         return self._shipment != Shipment.CHAT
     
@@ -112,6 +91,8 @@ class ModerationCard(Card):
         category_id: int,
         unit_price: Money,
         init_price: Money,
+        name: str,
+        body: str,
         features: dict,
         action_time: ActionTime,
         shipment: Shipment,
@@ -125,60 +106,26 @@ class ModerationCard(Card):
         self._category_id = category_id
         self._unit_price = unit_price
         self._init_price = init_price
+        self._name = name
+        self._body = body
         self._features = features
         self._action_time = action_time
         self._shipment = shipment
         self._created_at = created_at
         self._status = status
         self._inventory_entries = inventory_entries
-        self._descriptions: list[Description] = []
 
-    def accept(self, current_time: datetime) -> None:
-        match self._status:
-            case StatusCard.ON_FIRST_MODERATION:
-                self._descriptions[0].set(current_time)
-
-            case StatusCard.ON_MODERATION:
-                for description in self._descriptions:
-                    if description.status == StatusDescription.NEW:
-                        description.set(current_time)
-                    
-                    if description.status == StatusDescription.CURRENT:
-                        description.archive(current_time)
-
+    def accept(self) -> None:
         self._status = StatusCard.ON_SALE
 
-    def reject(self, reason: str, occurred_at: datetime) -> None:
-        match self._status:
-            case StatusCard.ON_FIRST_MODERATION:
-                self._status = StatusCard.REJECTED
-            
-            case StatusCard.ON_MODERATION:
-                for description in self._descriptions:
-                    if description.status == StatusDescription.NEW:
-                        description.cancel()
-                
-                self._status = StatusCard.ON_SALE
-
+    def reject(self, reason: str, occurred_at: datetime) -> None:        
+        self._status = StatusCard.ON_SALE
         self.add_event(
             AdminRejectedModerationCardNotification(
                 self._card_id,
                 self._status,
                 reason,
                 occurred_at
-            )
-        )
-
-    def add_desciption(self, name: str, body: str) -> None:
-        for field in [name, body]:
-            if len(field) < 10:
-                raise DomainError()
-            
-        self._descriptions.append(
-            Description(
-                name,
-                body,
-                StatusDescription.NEW
             )
         )
 
@@ -215,12 +162,8 @@ class ModerationCard(Card):
     def action_time(self) -> ActionTime:
         return self._action_time
     
-    @property
-    def descriptions(self) -> list[Description]:
-        return self._descriptions
-    
 
-class PurchasedCard:
+class PurchasedCard(Card):
     def __init__(
         self,
         card_id: int,
